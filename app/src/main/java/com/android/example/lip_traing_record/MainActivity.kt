a5b33b2b73de6140d0ea9e8c642a4711c1a68746
John Doe <101156679+nix0617@users.noreply.github.com>
2023-02-02 13:08:30 +0800

    Initial commit

---
+++ app/src/main/java/com/android/example/lip_traing_record/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.example.lip_traing_record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
